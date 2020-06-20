4a1dbdad84beaaed134f2444e2247ca7ac095edb
workHub <klinsmann.carhuas@intercorpretial.pe>
2020-06-19 19:33:22 -0500

    Add comments in endpoint for swagger

--- backend/premium.calculator.api/Controllers/PremiumController.cs
+++ backend/premium.calculator.api/Controllers/PremiumController.cs
@@ -4,21 +4,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace premium.calculator.api.Controllers
 {
+
     public class PremiumController : ControllerBase
     {
+        /// <summary>
+        /// Match the premium value with criteria wildcard data
+        /// </summary>
+        /// <param name="dateOfBirth">Date Of Birth</param>
+        /// <param name="state">State of U.S.</param>
+        /// <param name="age">Age</param>
         [HttpGet("/api/cignium/v1/premium-calculator")]
-        public IActionResult Get([FromHeader] DateTime DateOfBirth,
-                                            [FromHeader] string State,
-                                            [FromHeader] int Age)
+        public IActionResult Get([FromHeader] DateTime dateOfBirth,
+                                            [FromHeader] string state,
+                                            [FromHeader] int age)
         {
 
             Entity.PremiumResponse _result = new Entity.PremiumResponse();
             try
             {
                 UseCase.Premium _premium = new UseCase.Premium();
-                _result = _premium.Get(DateOfBirth, State, Age);
+                _result = _premium.Get(dateOfBirth, state, age);
             }
-            catch(HttpException ex)
+            catch (HttpException ex)
             {
                 return this.StatusCode((int)ex.StatusCode, ex.Message);
             }
